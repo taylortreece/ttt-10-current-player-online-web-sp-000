f030ae7ebc35857b01068ecc7f126427e76e32f0
Taylor Treece <tayloratreece@gmail.com>
2020-12-10 13:35:21 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,15 +10,7 @@ board = [
   " "
 ]
 
+counter = 0
 def turn_count(board)
   board.each do |move|
-    count = 0
-    count += 1
-    if count % 2 == 0
-      board[move] = "O"
-    else
-      board[move] = "X"
-    end
-    return count
-  end
-end
+  if 
